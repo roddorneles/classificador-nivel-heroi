ca14540f12eb41776243a679f4b48d2e6765ab74
roddorneles <rodrigodornelesferreira@usp.br>
2023-10-25 20:27:33 -0300

    implementa logica de classificacao do heroi

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 
 const nome = "Rodrigo";
-const xp = 1500
+const xp = 2001;
 
-let mensagem = ""
+let msg = "";
 
-if (xp > 1000)
+// estruturas de if's aninhadas para verificar qual o nível do herói
+if  (xp < 1000){
+    msg = "Ferro";
+}
+else if (xp <= 2000 ){
+    msg = "Bronze";
+}
+else if (xp <= 5000 ){
+    msg = "Prata";
+}
+else if (xp <= 7000 ){
+    msg = "Ouro";
+}
+else if (xp <= 8000 ){
+    msg = "Platina";
+}
+else if (xp <= 9000 ){
+    msg = "Ascendente";
+}
+else if (xp <= 10000 ){
+    msg = "Imortal";
+}
+else{
+    msg = "Radiante";
+}
+
+console.log (`O Herói de nome **${nome}** está no nível de **${msg}**`)
